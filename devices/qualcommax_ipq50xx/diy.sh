37e08db420551f4afea0eea602548f71c462fabd
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-02-14 02:24:35 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -8,4 +8,4 @@ rm -rf package/boot/uboot-envtools package/firmware/ipq-wifi target/linux/qualco
 
 git_clone_path main https://github.com/openwrt/openwrt package/boot/uboot-envtools package/firmware/ipq-wifi package/kernel/mac80211 package/kernel/qca-ssdk package/kernel/qca-nss-dp
 
-git_clone_path f76b846eea288be452be070413353185e7abe822 https://github.com/openwrt/openwrt target/linux/qualcommax
+git_clone_path 411df8fbc449175b0f46cf0dab229a16176ed067 https://github.com/openwrt/openwrt target/linux/qualcommax
