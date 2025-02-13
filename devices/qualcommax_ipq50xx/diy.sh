856746f7ed6f79a6e36dc9e3dbe7296c295999a6
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-02-14 02:10:04 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -4,8 +4,8 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/boot/uboot-envtools package/firmware/ipq-wifi target/linux/qualcommax package/kernel/mac80211 package/kernel/qca-ssdk
+rm -rf package/boot/uboot-envtools package/firmware/ipq-wifi target/linux/qualcommax package/kernel/mac80211 package/kernel/qca-ssdk package/kernel/qca-nss-dp
 
-git_clone_path main https://github.com/openwrt/openwrt package/boot/uboot-envtools package/firmware/ipq-wifi package/kernel/mac80211 package/kernel/qca-ssdk
+git_clone_path main https://github.com/openwrt/openwrt package/boot/uboot-envtools package/firmware/ipq-wifi package/kernel/mac80211 package/kernel/qca-ssdk package/kernel/qca-nss-dp
 
 git_clone_path f76b846eea288be452be070413353185e7abe822 https://github.com/openwrt/openwrt target/linux/qualcommax
